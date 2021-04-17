e98cf9584f93cd309451753cf6a346ab32dc9279
Shaminda <shaminda1017@gmail.com>
2021-04-17 02:20:27 -0400

    added video link

--- react-client/src/components/MotivationVideo.js
+++ react-client/src/components/MotivationVideo.js
@@ -1,19 +1,38 @@
 import React from "react";
 
 function MotivationVideo(prob) {
-
    
-   const url = "https://youtu.be/y8FsQTPqwUA";
+  const url1 = "https://www.youtube.com/embed/PgQ3sW0BWfI";
+  const url2 = "https://www.youtube.com/embed/XSo1brg8pT0";
+  const url3 = "https://www.youtube.com/embed/T1XLHsz4r_M";
+  const gameurl = "https://dami908.github.io/Micro-Garrisson/"; 
   return (
-    <div className="App">      
+    <div className="App"> 
+       {/* <video    
         src={url}
-        width='100%'
-            height='100%'
-            controls = {true}
+        width='50%'
+            height='50%'
+            controls = {true}s
         frameborder="0"
         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen       
-    </div>
+        allowfullscreen    />    */}
+        {/* <video width="320" height="240" controls> 
+        <source src={url}/> </video> */}
+      <div>
+      <iframe title="very exercise motivational game copywrite emmanuel ajay" width="1100" height="900" src={gameurl}> </iframe>
+      </div>
+      <div>
+      <iframe title="generic exercise motivation 1" width="1100" height="900" src={url1}> </iframe>
+      </div>
+      <div>
+      <iframe title="genric exercise motivation 2" width="1100" height="900" src={url2}> </iframe>
+      </div>
+      <div>
+      <iframe title="genric exercise motivation 3" width="1100" height="900" src={url3}> </iframe>
+      </div>
+  </div>
   );
+    
+    
 }
 export default MotivationVideo;
